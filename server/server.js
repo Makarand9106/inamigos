147d7403efbf0974e9f236feffd44dbbec8916f8
Makarand9106 <makarand9359@gmail.com>
2026-05-20 14:14:37 +0530

    Changed version of ui

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ const seedData = async () => {
 
 seedData();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
